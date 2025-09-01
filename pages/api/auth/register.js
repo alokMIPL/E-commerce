1408769966b7316080897c29a5110eb821fea298
alokMIPL <alok@madhulata.com>
2025-09-01 09:53:04 +0530

    code added

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,8 +1,9 @@
 import dbConnect from "@/backend/config/dbConnect"
 import { registerUser } from "@/backend/controllers/authControllers";
 import { default as nc } from "next-connect";
+import onError from "@/backend/middlewares/errors"
 
-const handler = nc();
+const handler = nc( { onError } );
 
 dbConnect();
 
