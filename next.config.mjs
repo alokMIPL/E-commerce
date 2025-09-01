071ef281a0e65805a153416721c620e06367de14
alokMIPL <alok@madhulata.com>
2025-09-01 13:33:12 +0530

    code added

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,11 @@ const nextConfig = {
   env:{
     API_URL:"http://localhost:3000",
     DB_URI:"mongodb+srv://alok:alok12@cluster0.aml5dsl.mongodb.net/ecommerce",
+
+    CLOUD_NAME: "dc0wr8hev",
+    CLOUDINARY_API_KEY: "564466854761365",
+    CLOUDINARY_API_SECRET: "2SxZlwIQSG8-jsYrikGpQgaYUXo",
+
     NEXTAUTH_SECRET:"asfdasdgs!@##$2342152345"
   },
   images:{
